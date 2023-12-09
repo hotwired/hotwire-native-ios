b9d397c634ffde620861cd7cd5615c7cb66dd480
Joe Masilotti <joe@masilotti.com>
2023-12-09 07:09:52 -0800

    Move AppDelegate into correct directory

---
+++ Demo/AppDelegate.swift
@@ -0,0 +1,20 @@
+import Hotwire
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        #if DEBUG
+        Turbo.config.debugLoggingEnabled = true
+        Strada.config.debugLoggingEnabled = true
+        #endif
+
+        return true
+    }
+
+    // MARK: UISceneSession Lifecycle
+
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+}
