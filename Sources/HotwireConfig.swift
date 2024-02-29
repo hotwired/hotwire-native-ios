f99ea861e1bfcb1ce8a4ee32c9f02badfb2879ef
Joe Masilotti <joe@masilotti.com>
2024-02-29 11:46:45 -0800

    Use internal values for re-exposed configuration

--- Sources/HotwireConfig.swift
+++ Sources/HotwireConfig.swift
@@ -13,28 +13,24 @@ public struct HotwireConfig {
 
     /// Override to set a custom user agent.
     /// - Important: Include "Turbo Native" to use `turbo_native_app?` on your Rails server.
-    public var userAgent = "Turbo Native iOS" {
+    public var userAgent = Turbo.config.userAgent {
         didSet { Turbo.config.userAgent = userAgent }
     }
 
     /// The view controller used in `TurboNavigator` for web requests. Must be
     /// a `VisitableViewController` or subclass.
-    public var defaultViewController: (URL) -> VisitableViewController = { url in
-        VisitableViewController(url: url)
-    } {
+    public var defaultViewController = Turbo.config.defaultViewController {
         didSet { Turbo.config.defaultViewController = defaultViewController }
     }
 
     /// Optionally customize the web views used by each Turbo Session.
     /// Ensure you return a new instance each time.
-    public var makeCustomWebView: TurboConfig.WebViewBlock = { (configuration: WKWebViewConfiguration) in
-        WKWebView(frame: .zero, configuration: configuration)
-    } {
+    public var makeCustomWebView = Turbo.config.makeCustomWebView {
         didSet { Turbo.config.makeCustomWebView = makeCustomWebView }
     }
 
     /// Enable or disable debug logging for Turbo visits.
-    public var turboDebugLoggingEnabled = false {
+    public var turboDebugLoggingEnabled = Turbo.config.debugLoggingEnabled {
         didSet { Turbo.config.debugLoggingEnabled = turboDebugLoggingEnabled }
     }
 
@@ -43,20 +39,20 @@ public struct HotwireConfig {
     /// Set a custom JSON encoder when parsing bridge payloads.
     /// The custom encoder can be useful when you need to apply specific
     /// encoding strategies, like snake case vs. camel case
-    public var bridgeJsonEncoder = JSONEncoder() {
+    public var bridgeJsonEncoder = Strada.config.jsonEncoder {
         didSet { Strada.config.jsonEncoder = bridgeJsonEncoder }
     }
 
     /// Set a custom JSON decoder when parsing bridge payloads.
-    /// The custom encoder can be useful when you need to apply specific
-    /// encoding strategies, like snake case vs. camel case
-    public var bridgeJsonDecoder = JSONDecoder() {
+    /// The custom decoder can be useful when you need to apply specific
+    /// decoding strategies, like snake case vs. camel case
+    public var bridgeJsonDecoder = Strada.config.jsonDecoder {
         didSet { Strada.config.jsonDecoder = bridgeJsonDecoder }
     }
 
     /// Enable or disable debug logging for bridge elements connecting,
     /// disconnecting, receiving/sending messages, and more.
-    public var bridgeDebugLoggingEnabled = false {
+    public var bridgeDebugLoggingEnabled = Strada.config.debugLoggingEnabled {
         didSet { Strada.config.debugLoggingEnabled = bridgeDebugLoggingEnabled }
     }
 }
