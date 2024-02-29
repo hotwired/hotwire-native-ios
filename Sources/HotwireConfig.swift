49e9c5e6d9c96dd791b6985e75eb8366b4b1ac8c
Joe Masilotti <joe@masilotti.com>
2024-02-29 11:35:37 -0800

    Wrap Strada config in Hotwire config

--- Sources/HotwireConfig.swift
+++ Sources/HotwireConfig.swift
@@ -7,4 +7,26 @@ public struct HotwireConfig {
 
     /// Sets the back button display mode of `HotwireWebViewController`.
     public var backButtonDisplayMode = UINavigationItem.BackButtonDisplayMode.default
+
+    // MARK: Bridge
+
+    /// Set a custom JSON encoder when parsing bridge payloads.
+    /// The custom encoder can be useful when you need to apply specific
+    /// encoding strategies, like snake case vs. camel case
+    public var bridgeJsonEncoder = JSONEncoder() {
+        didSet { Strada.config.jsonEncoder = bridgeJsonEncoder }
+    }
+
+    /// Set a custom JSON decoder when parsing bridge payloads.
+    /// The custom encoder can be useful when you need to apply specific
+    /// encoding strategies, like snake case vs. camel case
+    public var bridgeJsonDecoder = JSONDecoder() {
+        didSet { Strada.config.jsonDecoder = bridgeJsonDecoder }
+    }
+
+    /// Enable or disable debug logging for bridge elements connecting,
+    /// disconnecting, receiving/sending messages, and more.
+    public var bridgeDebugLoggingEnabled = false {
+        didSet { Strada.config.debugLoggingEnabled = bridgeDebugLoggingEnabled }
+    }
 }
