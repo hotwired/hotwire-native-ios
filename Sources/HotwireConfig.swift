48b95c5f66514364acd491075cb509f9c657aef8
Joe Masilotti <joe@masilotti.com>
2024-02-29 11:27:55 -0800

    Default to NOT showing done buttons on modals

--- Sources/HotwireConfig.swift
+++ Sources/HotwireConfig.swift
@@ -1,5 +1,5 @@
 public struct HotwireConfig {
     /// When enabled, adds a `UIBarButtonItem` of type `.done` to the left
     /// navigation bar button item on screens presented modally.
-    public var showDoneButtonOnModals = true
+    public var showDoneButtonOnModals = false
 }
