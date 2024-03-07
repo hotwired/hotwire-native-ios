d55bbfe7b3f95db9871ad6f00ed68133b2dc0e0b
Joe Masilotti <joe@masilotti.com>
2024-03-07 11:21:49 -0800

    Reconfigure to match Turbo demo app

--- Demo/AppDelegate.swift
+++ Demo/AppDelegate.swift
@@ -4,6 +4,9 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        Hotwire.config.backButtonDisplayMode = .minimal
+        Hotwire.config.showDoneButtonOnModals = true
+
         #if DEBUG
         Hotwire.config.debugLoggingEnabled = true
         #endif
