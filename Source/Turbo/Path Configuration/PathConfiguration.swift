ac59bce66f6cbc91512b37b67e80b9a8879993b5
Joe Masilotti <joe@masilotti.com>
2024-03-23 16:15:40 -0600

    Backport turbo-ios PR #190
    https://github.com/hotwired/turbo-ios/pull/190

--- Source/Turbo/Path Configuration/PathConfiguration.swift
+++ Source/Turbo/Path Configuration/PathConfiguration.swift
@@ -11,7 +11,7 @@ public struct PathConfigurationLoaderOptions {
     public init(urlSessionConfiguration: URLSessionConfiguration? = nil) {
         self.urlSessionConfiguration = urlSessionConfiguration
     }
-    
+
     /// If present, the ``PathConfigurationLoader`` will initialize a new `URLSession` with
     /// this configuration to make its network request
     public let urlSessionConfiguration: URLSessionConfiguration?
@@ -19,13 +19,13 @@ public struct PathConfigurationLoaderOptions {
 
 public final class PathConfiguration {
     public weak var delegate: PathConfigurationDelegate?
-    
+
     /// Returns top-level settings: `{ settings: {} }`
     public private(set) var settings: [String: AnyHashable] = [:]
-    
+
     /// The list of rules from the configuration: `{ rules: [] }`
     public private(set) var rules: [PathRule] = []
-    
+
     /// Sources for this configuration, setting it will
     /// cause the configuration to be loaded from the new sources
     public var sources: [Source] = [] {
@@ -33,7 +33,7 @@ public final class PathConfiguration {
             load()
         }
     }
-    
+
     /// Multiple sources will be loaded in order
     /// Remote sources should be last since they're loaded async
     public init(sources: [Source] = [], options: PathConfigurationLoaderOptions? = nil) {
@@ -41,51 +41,50 @@ public final class PathConfiguration {
         self.options = options
         load()
     }
-    
+
     /// Convenience method for getting properties for path: configuration["/path"]
     public subscript(path: String) -> PathProperties {
         properties(for: path)
     }
-    
+
     /// Convenience method for retrieving properties for url: configuration[url]
     public subscript(url: URL) -> PathProperties {
         properties(for: url)
     }
-    
-    /// Returns a merged dictionary containing all the properties
-    /// that match this url
-    /// Note: currently only looks at path, not query, but most likely will
-    /// add query support in the future, so it's best to always use this over the path variant
-    /// unless you're sure you'll never need to reference other parts of the URL in the future
+
+    /// Returns a merged dictionary containing all the properties that match this URL.
     public func properties(for url: URL) -> PathProperties {
-        properties(for: url.path)
+        if Turbo.config.pathConfiguration.matchQueryStrings, let query = url.query {
+            return properties(for: "\(url.path)?\(query)")
+        }
+        return properties(for: url.path)
     }
-    
+
     /// Returns a merged dictionary containing all the properties
     /// that match this path
     public func properties(for path: String) -> PathProperties {
         var properties: PathProperties = [:]
-        
+
         for rule in rules where rule.match(path: path) {
             properties.merge(rule.properties) { _, new in new }
         }
-        
+
         return properties
     }
-    
+
     // MARK: - Loading
-    
+
     private let options: PathConfigurationLoaderOptions?
 
     private var loader: PathConfigurationLoader?
-    
+
     private func load() {
         loader = PathConfigurationLoader(sources: sources, options: options)
         loader?.load { [weak self] config in
             self?.update(with: config)
         }
     }
-    
+
     private func update(with config: PathConfigurationDecoder) {
         // Update our internal state with the config from the loader
         settings = config.settings
