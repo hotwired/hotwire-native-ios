f80402468b1d3e69eb6bc262a6aa52147fba14f8
Joe Masilotti <joe@masilotti.com>
2024-04-06 06:21:54 -0700

    Remove reference to file that no longer exists

--- Tests/Turbo/VisitOptionsTests.swift
+++ Tests/Turbo/VisitOptionsTests.swift
@@ -23,18 +23,6 @@ class VisitOptionsTests: XCTestCase {
     func test_Decodable_canBeInitializedWithResponse() throws {
         _ = try validVisitVisitOptions(responseHTMLString: "<html></html>")
     }
-
-    func test_visitOptionsArePreserved() throws {
-        let visitOptionsWithResponse = try validVisitVisitOptions(responseHTMLString: "<html></html>")
-        let handler = VisitOptionsHandler()
-
-        let processedOptions = handler.process(visitOptionsWithResponse)
-        XCTAssert(processedOptions == visitOptionsWithResponse)
-
-        let nextVisitOptions = try validVisitVisitOptions(responseHTMLString: nil)
-        let savedOptions = handler.process(nextVisitOptions)
-        XCTAssert(savedOptions == visitOptionsWithResponse)
-    }
 }
 
 extension VisitOptionsTests {
