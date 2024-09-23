23b5ae3f5f44c1b1c2e187728b6e853d951c3b5e
Fernando Olivares <fernandoolivares@37signals.com>
2024-09-23 14:54:14 -0600

    Rename basic to remote

--- Demo/Demo.swift
+++ Demo/Demo.swift
@@ -1,11 +1,11 @@
 import Foundation
 
 struct Demo {
-    static let basic = URL(string: "https://hotwire-native-demo.dev/")!
+    static let remote = URL(string: "https://hotwire-native-demo.dev/")!
     static let local = URL(string: "http://localhost:45678")!
 
     /// Update this to choose which demo is run
     static var current: URL {
-        basic
+        remote
     }
 }
