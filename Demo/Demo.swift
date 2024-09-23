4e719b28490cf1ef19e234ec3662f3c34899fa41
Fernando Olivares <fernandoolivares@37signals.com>
2024-09-23 12:35:52 -0600

    Move to hotwire-native-demo and remove some Turbolinks 5 references

--- Demo/Demo.swift
+++ Demo/Demo.swift
@@ -1,11 +1,8 @@
 import Foundation
 
 struct Demo {
-    static let basic = URL(string: "https://turbo-native-demo.glitch.me")!
-    static let turbolinks5 = URL(string: "https://turbo-native-demo.glitch.me?turbolinks=1")!
-
+    static let basic = URL(string: "https://hotwire-native-demo.dev/")!
     static let local = URL(string: "http://localhost:45678")!
-    static let turbolinks5Local = URL(string: "http://localhost:45678?turbolinks=1")!
 
     /// Update this to choose which demo is run
     static var current: URL {
