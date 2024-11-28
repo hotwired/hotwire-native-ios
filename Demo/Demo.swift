8c221611b2463909b8c4c4be4e9a186fae6941f3
Yaro Shm <yshmarov@gmail.com>
2024-11-28 10:23:24 +0100

    use localhost 3000

--- Demo/Demo.swift
+++ Demo/Demo.swift
@@ -2,10 +2,10 @@ import Foundation
 
 struct Demo {
     static let remote = URL(string: "https://hotwire-native-demo.dev")!
-    static let local = URL(string: "http://localhost:45678")!
+    static let local = URL(string: "http://localhost:3000")!
 
     /// Update this to choose which demo is run
     static var current: URL {
-        remote
+        local
     }
 }
