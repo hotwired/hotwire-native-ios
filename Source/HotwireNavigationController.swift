ddae7938907726bb917a0823e9a63a07962e86e5
Denis Švara <denis.svara@gmail.com>
2024-12-03 14:28:07 +0100

    Document `HotwireNavigationController`.

--- Source/HotwireNavigationController.swift
+++ Source/HotwireNavigationController.swift
@@ -1,5 +1,30 @@
 import UIKit
 
+/// The `HotwireNavigationController` is a custom subclass of `UINavigationController` designed to enhance the management of `VisitableViewController` instances within a navigation stack.
+/// It tracks the reasons why a view controller appears or disappears, which is crucial for handling navigation in Hotwire-powered applications.
+/// - Important: If you are using a custom or third-party navigation controller, subclass `HotwireNavigationController` to integrate its behavior.
+///
+/// ## Usage Notes
+///
+/// - **Integrating with Custom Navigation Controllers:**
+///   If you're using a custom or third-party navigation controller, subclass `HotwireNavigationController` to incorporate the necessary behavior.
+///
+///   ```swift
+///   open class YourCustomNavigationController: HotwireNavigationController {
+///       // Make sure to always call super when overriding functions from `HotwireNavigationController`.
+///   }
+///   ```
+///
+/// - **Extensibility:**
+///   The class is marked as `open`, allowing you to subclass and extend its functionality to suit your specific needs.
+///
+/// ## Limitations
+///
+/// - **Other Container Controllers:**
+///   The current implementation focuses on `UINavigationController` and includes handling for `UITabBarController`. It does not provide out-of-the-box support for other container controllers like `UISplitViewController`.
+///
+/// - **Custom Navigation Setups:**
+///   For completely custom navigation setups or container controllers, you will need to implement similar logic to manage the `appearReason` and `disappearReason` of `VisitableViewController` instances.
 open class HotwireNavigationController: UINavigationController {
     open override func pushViewController(_ viewController: UIViewController, animated: Bool) {
         if let visitableViewController = viewController as? VisitableViewController {
