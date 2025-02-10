5b0c6f783bc13158702a8f3170a7a2dea5f31ad8
Joe Masilotti <joe@masilotti.com>
2025-02-10 15:09:01 -0800

    Realign tabs with server names

--- Demo/Tab.swift
+++ Demo/Tab.swift
@@ -15,6 +15,6 @@ struct Tab {
     static let all = [
         Tab(title: "Navigation", imageName: "arrow.left.arrow.right", path: nil),
         Tab(title: "Bridge Components", imageName: "widget.small", path: "components"),
-        Tab(title: "Reference", imageName: "questionmark.text.page", path: "references"),
+        Tab(title: "Resources", imageName: "questionmark.text.page", path: "resources"),
     ]
 }
