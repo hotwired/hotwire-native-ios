e976885e44f2065ee76d1808069c5563d39b11e1
Denis Švara <denis.svara@gmail.com>
2025-03-24 22:00:08 +0100

    Implement `SafariViewControllerRouteDecisionHandler`.

---
+++ Source/Turbo/Navigator/Routing/Handlers/SafariViewControllerRouteDecisionHandler.swift
@@ -0,0 +1,40 @@
+import Foundation
+import WebKit
+import SafariServices
+
+final class SafariViewControllerRouteDecisionHandler: RouteDecisionHandler {
+    let name: String = "safari"
+
+    func matches(location: URL,
+                 configuration: Navigator.Configuration) -> Bool {
+        if #available(iOS 16, *) {
+            return configuration.startLocation.host() != location.host()
+        }
+
+        return configuration.startLocation.host != location.host
+    }
+
+    func handle(location: URL,
+                configuration: Navigator.Configuration,
+                navigator: Navigator) -> Router.Decision {
+        open(externalURL: location,
+             viewController: navigator.activeNavigationController)
+
+        return .cancel
+    }
+
+    func open(externalURL: URL,
+              viewController: UIViewController) {
+
+        /// SFSafariViewController will crash if we pass along a URL that's not valid.
+        guard externalURL.scheme == "http" || externalURL.scheme == "https" else { return }
+
+        let safariViewController = SFSafariViewController(url: externalURL)
+        safariViewController.modalPresentationStyle = .pageSheet
+        if #available(iOS 15.0, *) {
+            safariViewController.preferredControlTintColor = .tintColor
+        }
+
+        viewController.present(safariViewController, animated: true)
+    }
+}
