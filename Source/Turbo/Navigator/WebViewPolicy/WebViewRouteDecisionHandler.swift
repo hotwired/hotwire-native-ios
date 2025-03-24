b5a73c7f581f00b0963a0593c019e5424b9bef7d
Denis Švara <denis.svara@gmail.com>
2025-03-24 21:41:49 +0100

    Move `WKNavigationAction` utils to appropriate extension.

--- Source/Turbo/Navigator/WebViewPolicy/WebViewRouteDecisionHandler.swift
+++ Source/Turbo/Navigator/WebViewPolicy/WebViewRouteDecisionHandler.swift
@@ -11,25 +11,3 @@ public protocol WebViewPolicyDecisionHandler {
                 configuration: Navigator.Configuration,
                 navigator: Navigator) -> WebViewPolicyManager.Decision
 }
-
-extension WKNavigationAction {
-    var shouldNavigateInApp: Bool {
-        navigationType == .linkActivated ||
-        isMainFrameNavigation
-    }
-
-    /// Indicates if the navigation action requests a new window (e.g., target="_blank").
-    var requestsNewWindow: Bool {
-        guard let targetFrame else { return true }
-        return !targetFrame.isMainFrame
-    }
-
-    var shouldReloadPage: Bool {
-        return isMainFrameNavigation && navigationType == .reload
-    }
-
-    var shouldOpenURLExternally: Bool {
-        return navigationType == .linkActivated ||
-        (isMainFrameNavigation && navigationType == .other)
-    }
-}
