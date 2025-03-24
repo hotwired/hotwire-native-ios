c26fd3326af1c0ced54bc9a91baf6c92955ccf77
Denis Švara <denis.svara@gmail.com>
2025-03-24 21:52:32 +0100

    Remove web view related code from Router.

--- Source/Turbo/Navigator/Routing/Router.swift
+++ Source/Turbo/Navigator/Routing/Router.swift
@@ -1,21 +1,17 @@
 import Foundation
-import WebKit
 
 /// Routes location urls within in-app navigation or with custom behaviors
 /// provided in `RouteDecisionHandler` instances.
 public final class Router {
     let decisionHandlers: [RouteDecisionHandler]
-    let webViewDecisionHandlers: [WebViewRouteDecisionHandler]
 
-    init(decisionHandlers: [RouteDecisionHandler],
-         webViewDecisionHandlers: [WebViewRouteDecisionHandler]) {
+    init(decisionHandlers: [RouteDecisionHandler]) {
         self.decisionHandlers = decisionHandlers
-        self.webViewDecisionHandlers = webViewDecisionHandlers
     }
 
     func decideRoute(for location: URL,
                      configuration: Navigator.Configuration,
-                     navigator: Navigator) -> Decision {
+                     navigator: Navigator) -> Router.Decision {
         for handler in decisionHandlers {
             if handler.matches(location: location, configuration: configuration) {
                 logger.debug("[Router] handler match found handler: \(handler.name) location: \(location)")
@@ -27,22 +23,6 @@ public final class Router {
 
         logger.warning("[Router] no handler for location: \(location)")
         return .cancel
-    }
-
-    func decidePolicy(for navigationAction: WKNavigationAction,
-                      configuration: Navigator.Configuration,
-                      navigator: Navigator) -> Decision {
-        for handler in webViewDecisionHandlers {
-            if handler.matches(navigationAction: navigationAction, configuration: configuration) {
-                logger.debug("[Router] web handler match found handler: \(handler.name) navigation action:\(navigationAction)")
-                return handler.handle(navigationAction: navigationAction,
-                               configuration: configuration,
-                               navigator: navigator)
-            }
-        }
-
-        logger.warning("[Router] no web handler for navigation action: \(navigationAction)")
-        return .navigate
     }
 }
 
