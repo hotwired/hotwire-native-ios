289799edaa8ac85b017303e662c8a5509d1f5307
Denis Švara <denis.svara@gmail.com>
2025-03-25 08:46:08 +0100

    Add `SafariViewControllerRouteDecisionHandler` tests.

---
+++ Tests/Turbo/Routing/SafariViewControllerRouteDecisionHandlerTests.swift
@@ -0,0 +1,44 @@
+@testable import HotwireNative
+import WebKit
+import XCTest
+
+final class SafariViewControllerRouteDecisionHandlerTests: XCTestCase {
+    let navigatorConfiguration = Navigator.Configuration(
+        name: "test",
+        startLocation: URL(string: "https://my.app.com")!
+    )
+    var navigator: Navigator!
+    var route: BrowserRouteDecisionHandler!
+
+    override func setUp() {
+        route = BrowserRouteDecisionHandler()
+        navigator = Navigator(configuration: navigatorConfiguration)
+    }
+
+    func test_handling_matching_result_stops_navigation() {
+        let url = URL(string: "https://external.com/page")!
+        let result = route.handle(location: url, configuration: navigatorConfiguration, navigator: navigator)
+        XCTAssertEqual(result, Router.Decision.cancel)
+    }
+
+    func test_url_on_external_domain_matches() {
+        let url = URL(string: "https://external.com/page")!
+        let result = route.matches(location: url, configuration: navigatorConfiguration)
+
+        XCTAssertTrue(result)
+    }
+
+    func test_url_without_subdomain_matches() {
+        let url = URL(string: "https://app.com/page")!
+        let result = route.matches(location: url, configuration: navigatorConfiguration)
+
+        XCTAssertTrue(result)
+    }
+
+    func test_url_on_app_domain_does_not_match() {
+        let url = URL(string: "https://my.app.com/page")!
+        let result = route.matches(location: url, configuration: navigatorConfiguration)
+
+        XCTAssertFalse(result)
+    }
+}
